ce4be2dc25ea5fb9a912969ed4180c36a08297c8
Sarim Mehdi <sarim.mehdi.550@gmail.com>
2020-06-26 13:09:31 +0200

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -21,118 +21,81 @@ def main():
 	rospy.Subscriber('/odom', Odometry, myRobot.updateRobotPos)
 	vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 	viz_publisher = rospy.Publisher('/visualization_marker_array', MarkerArray, queue_size=10)
+	pt_publisher = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
 
 	rospy.loginfo("WAITING FOR ROBOT POSITION...")
 	while not myRobot.gotRobotPos:
 		rospy.sleep(0.01)
 	rospy.loginfo("GOT ROBOT POSITION!")
+	
+	#dummy loop to wait for laser scanner to give a valid first point
+	time = rospy.get_time()
+	while rospy.get_time() - time <= 3:
+		markerID = 0
 
+	tryAgain = False
 	vel_msg = Twist()
-	gotviaPt = False
-	recentPos = myRobot.curPos
-	reverse = False
+	markerID = 0
+	obsMarkerList = []
 
-	#make one or both of these true if you are trying to observe other robot behaviors
-	#useful if you want to see whether the robot successfully turns towards a goal or not 
-	dontMove = False
-	dontRot = False
 	while not rospy.is_shutdown():
+		success = False
 		markerArray = MarkerArray()
 
-		#erase previous markers to draw new ones
+		#erase previous line markers to draw new ones
 		if markerArray.markers:
 			for marker in markerArray.markers:
 				marker.action = marker.DELETE	
 			viz_publisher.publish(markerArray)
 			markerArray.markers[:] = []
 
-		#draw points
-		#markerArray.markers.append(createLine(myRobot.curPos, myRobot.farthestObsInFront, 0))
-		markerArray.markers.append(createLine(myRobot.curPos, myRobot.viaPt, 1))
-		viz_publisher.publish(markerArray)
-		if not gotviaPt:
-			rospy.loginfo("CURRENT ROBOT POSITION IS:")
-			rospy.loginfo(myRobot.curPos)
-			myRobot.viaPt = copy.deepcopy(myRobot.farthestObsInFront)
-			rospy.loginfo("GOT POSITION!")
-			rospy.loginfo(myRobot.viaPt)
-			posTimer = rospy.get_time()
-			recentPos = myRobot.curPos
-			gotviaPt = True
-			reverse = False
-		myRobot.movToAPoint(vel_msg, 0.2, 0.05, 0.5)
-
-		if np.linalg.norm(myRobot.curPos-recentPos) > 0.1:
-			recentPos = myRobot.curPos
-
-		if myRobot.closestDistToObs <= 0.4 and myRobot.obsDir == 'left':
-			vel_msg.angular.z = 1.0
-
-		if myRobot.closestDistToObs <= 0.4 and myRobot.obsDir == 'right':
-			vel_msg.angular.z = -1.0
-
-		if ((rospy.get_time() - posTimer >= 10.0 and np.linalg.norm(myRobot.curPos-recentPos) <= 0.1) or \
-			(myRobot.obsDir == 'both' and \
-			myRobot.minAngleToClosestObs in range(61) and \
-			myRobot.minAngleToClosestObs in range(360, 361))) and \
-			not reverse: 
-			reverse = True
-			reverseTimer = rospy.get_time()
-
-		if reverse and rospy.get_time() - reverseTimer > 5.0:
-			rospy.loginfo("ROBOT SHOULD BE OUT OF STUCK PLACE NOW...")
-			vel_msg.angular.z = 0
-			vel_msg.linear.x = 0
-			reverse = False
-			gotviaPt = False
-			posTimer = rospy.get_time()
-			rospy.loginfo("TRYING AGAIN")
-
-		if reverse:
-			rospy.loginfo("ROBOT IS STUCK SOMEWHERE! REVERSING!")
-			vel_msg.angular.z = 0
-			vel_msg.linear.x = -0.2
-
-		myRobot.checkIfPointBehind(vel_msg, 0.5)
-		if dontMove: vel_msg.linear.x = 0
-		if dontRot: vel_msg.angular.z = 0
-		vel_publisher.publish(vel_msg)
+		#draw a marker for every point you decide to move to
+		myRobot.getNewPt = True
+		while myRobot.getNewPt:
+			x = 2
+		rospy.loginfo("CURRENT ROBOT POSITION IS:")
+		rospy.loginfo(myRobot.curPos)
+		rospy.loginfo("GOT OBSTACLE POSITION TO FOLLOW:")
+		rospy.loginfo(myRobot.farthestObsAround)
+		obsMarker = createPoint(myRobot.farthestObsAround, markerID)
+		obsMarkerList.append(obsMarker)
+		pt_publisher.publish(obsMarker)
+		markerID += 1		
 		
-		rospy.sleep(0.01)
-		if np.linalg.norm(myRobot.curPos-myRobot.viaPt) <= 1.5:
-			myRobot.visitedPts.append(myRobot.viaPt)
-			if len(myRobot.visitedPts) >= 20: myRobot.visitedPts[:] = []
-			gotviaPt = False
+		#get intermediate points from robot's current position till goal position
+		myRobot.viaPts.append([myRobot.curPos[0], myRobot.curPos[1]])
+		if np.linalg.norm(myRobot.curPos-myRobot.farthestObsAround) > 0.1:
+			t = 0.1/(np.linalg.norm(myRobot.curPos-myRobot.farthestObsAround))
+			while t <= 1:
+				xt = (1-t)*myRobot.curPos[0] + t*myRobot.farthestObsAround[0]
+				yt = (1-t)*myRobot.curPos[1] + t*myRobot.farthestObsAround[1]
+				myRobot.viaPts.append([xt, yt])
+				t += t
+		myRobot.viaPts.append(copy.deepcopy([myRobot.farthestObsAround[0], myRobot.farthestObsAround[1]]))
+
+		#draw line markers
+		ID = 0
+		for i in range(len(myRobot.viaPts) - 1):
+			markerArray.markers.append(createLine(myRobot.viaPts[i], myRobot.viaPts[i+1], ID))
+			ID += 1
+		viz_publisher.publish(markerArray)
+
+		#follow points to goal
+		myRobot.followTrajectory(viz_publisher, vel_msg, vel_publisher, markerArray, tryAgain, success)
+		rospy.loginfo("ARRIVED AT POSITION, NOW GOING TO NEW ONE")
+		myRobot.visitedPts.append(myRobot.viaPts[-1])
+		myRobot.viaPts[:] = []
+		if len(myRobot.visitedPts) >= 60:
+			rospy.loginfo("MORE THAN 60 POINTS VISITED!")
+			myRobot.visitedPts[:] = []
+			for obsMark in obsMarkerList:
+				obsMark.action = obsMark.DELETE
+				pt_publisher.publish(obsMark)
+			markerID = 0
+			obsMarkerList[:] = []
 
 if __name__ == '__main__':
 	try:
 		main()
 	except rospy.ROSInterruptException:
 		pass
-
-			
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
